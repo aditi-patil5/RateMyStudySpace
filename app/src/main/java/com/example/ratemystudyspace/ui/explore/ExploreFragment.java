d24c24b0cb1b82ca2ece4a403350c9b5098880ed
Aditi Patil <aditivpatil5@gmail.com>
2023-03-21 21:01:57 -0500

    renamed navigation fragments and changed icons

--- app/src/main/java/com/example/ratemystudyspace/ui/explore/ExploreFragment.java
+++ app/src/main/java/com/example/ratemystudyspace/ui/explore/ExploreFragment.java
@@ -1,4 +1,4 @@
-package com.example.ratemystudyspace.ui.notifications;
+package com.example.ratemystudyspace.ui.explore;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,22 +10,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.ratemystudyspace.databinding.FragmentNotificationsBinding;
+import com.example.ratemystudyspace.databinding.FragmentExploreBinding;
 
-public class NotificationsFragment extends Fragment {
+public class ExploreFragment extends Fragment {
 
-    private FragmentNotificationsBinding binding;
+    private FragmentExploreBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        NotificationsViewModel notificationsViewModel =
-                new ViewModelProvider(this).get(NotificationsViewModel.class);
+        ExploreViewModel exploreViewModel =
+                new ViewModelProvider(this).get(ExploreViewModel.class);
 
-        binding = FragmentNotificationsBinding.inflate(inflater, container, false);
+        binding = FragmentExploreBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textNotifications;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        exploreViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
