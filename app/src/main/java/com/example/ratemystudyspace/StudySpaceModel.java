09601ee295539bc11f8fa6d78674b7a52ea340d4
Aditi Patil <aditivpatil5@gmail.com>
2023-03-28 13:34:35 -0500

    added variables in model class

--- app/src/main/java/com/example/ratemystudyspace/StudySpaceModel.java
+++ app/src/main/java/com/example/ratemystudyspace/StudySpaceModel.java
@@ -1,5 +1,7 @@
 package com.example.ratemystudyspace;
 
+import java.util.ArrayList;
+
 /**
  * This class represents StudySpaceFragment in code
  */
@@ -11,8 +13,64 @@ public class StudySpaceModel {
 
     private int imageMain;
 
+    private  ArrayList<String> reviews = new ArrayList<String>();
+
+    private boolean naturalLight;
+    private boolean outlets;
+    private boolean whiteboards;
+    private boolean isIndividual;
+    private String noiseLevel;
+
+    public boolean isNaturalLight() {
+        return naturalLight;
+    }
+
+    public void setNaturalLight(boolean naturalLight) {
+        this.naturalLight = naturalLight;
+    }
+
+    public boolean isOutlets() {
+        return outlets;
+    }
+
+    public void setOutlets(boolean outlets) {
+        this.outlets = outlets;
+    }
+
+    public boolean isWhiteboards() {
+        return whiteboards;
+    }
+
+    public void setWhiteboards(boolean whiteboards) {
+        this.whiteboards = whiteboards;
+    }
+
+    public boolean isIndividual() {
+        return isIndividual;
+    }
+
+    public void setIndividual(boolean individual) {
+        isIndividual = individual;
+    }
+
+    public String getNoiseLevel() {
+        return noiseLevel;
+    }
+
+    public void setNoiseLevel(String noiseLevel) {
+        this.noiseLevel = noiseLevel;
+    }
+
     public void setName(String name) {
         this.name = name;
+    }
+
+    public ArrayList<String> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(ArrayList<String> reviews) {
+        this.reviews = reviews;
     }
 
     public void setLocation(String location) {
@@ -50,6 +108,9 @@ public class StudySpaceModel {
         this.imageMain = imageMain;
     }
 
+    public void addReview(String review){
+        reviews.add(review);
+    }
 
 
 }
