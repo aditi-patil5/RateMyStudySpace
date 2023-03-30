fa0634f5f922b2532f81dea2c24ba8fed2750d48
Aditi Patil <aditivpatil5@gmail.com>
2023-03-30 13:47:33 -0500

    onclick for add review button

--- app/src/main/java/com/example/ratemystudyspace/ui/review/ReviewFragment.java
+++ app/src/main/java/com/example/ratemystudyspace/ui/review/ReviewFragment.java
@@ -11,12 +11,19 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.ratemystudyspace.MainActivity;
 import com.example.ratemystudyspace.R;
+import com.example.ratemystudyspace.databinding.FragmentFilterBinding;
+import com.example.ratemystudyspace.ui.space.SpaceFragment;
 
 public class ReviewFragment extends Fragment {
 
     private ReviewViewModel mViewModel;
+    private MainActivity mainActivity;
+
+    private Bundle args;
 
     public static ReviewFragment newInstance() {
         return new ReviewFragment();
@@ -25,14 +32,29 @@ public class ReviewFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_review, container, false);
+        View view = inflater.inflate(R.layout.fragment_review, container, false);
+        if(mainActivity == null){
+            mainActivity = (MainActivity) getActivity();
+        }
+
+        args = new Bundle();
+        args.putInt("image",getArguments().getInt("image"));
+        args.putString("name",getArguments().getString("name" ));
+        args.putString("location",getArguments().getString("location"));
+        args.putFloat("rating", getArguments().getFloat("rating"));
+        args.putString("reviews",getArguments().getString("reviews"));
+
+        Button submitBttn = view.findViewById(R.id.submit_review_add);
+        submitBttn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mainActivity.changeBottomNavigationTab(new SpaceFragment(),args);
+            }
+        });
+
+        return view;
+
     }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(ReviewViewModel.class);
-        // TODO: Use the ViewModel
-    }
 
 }
